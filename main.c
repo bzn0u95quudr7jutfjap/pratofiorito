8252d175e7653a09b95b11a3051563d73380551b
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-05 23:15:16 +0200

    tabella

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char table[12][12];
 
 int main(int argc, char ** argv){
-  printf("a\n");
+  srand(time(NULL));
+  printf("%d\n",rand());
   return 0;
 }
